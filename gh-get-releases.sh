593000630d1aefe76e2eff6497df74438895d051
Kirill Danshin <kirill@danshin.pro>
2020-07-24 18:11:20 +0300

    fix autoupdate name
    Signed-off-by: Kirill Danshin <kirill@danshin.pro>

--- gh-get-releases.sh
+++ gh-get-releases.sh
@@ -10,3 +10,5 @@ get_all_versions() {
     curl --silent "https://api.github.com/repos/pgbouncer/pgbouncer/releases" | jq -c '{ "tag": map(. .tag_name) }' \
         | sed s/_/./g | sed s/pgbouncer.//g | sed 's/,"1.11.0","1.10.0","1.9.0"//g'
 }
+
+echo get_all_versions
