7f8a801e5d42e0992a3e124fd7d408da30904ae9
Kirill Danshin <kirill@danshin.pro>
2020-07-24 14:56:36 +0300

    init
    Signed-off-by: Kirill Danshin <kirill@danshin.pro>

---
+++ gh-get-releases.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -a
+
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | jq -r ".tag_name"
+}
+
+get_all_versions() {
+    curl --silent "https://api.github.com/repos/pgbouncer/pgbouncer/releases" | jq -c '{ "tag": map(. .tag_name) }' \
+        | sed s/_/./g | sed s/pgbouncer.//g | sed 's/\.0"/"/g'
+}
